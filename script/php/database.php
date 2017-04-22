f515e021e033ba6c845ec93dd91004e8b2cd4bb5
Filippo Vajana <filippovajana@live.com>
2017-04-22 14:44:11 +0200

    changed to PDO

--- script/php/database.php
+++ script/php/database.php
@@ -1,9 +1,53 @@
-<?php    
+<?php  
   class Database
   {
-    function PdoConnectAdmin()
+    function GetUserCredentials($user)
     {
-      
+        //read JSON
+        $json = file_get_contents('credentials.json');
+
+        //decode
+        $json_data = json_decode($json, true);
+
+        //get user entry
+        foreach($json_data as $key => $value)
+        {
+            if($json_data[$key]["Username"] == $user)
+            {
+                return $json_data[$key];
+            }
+        }
+    }
+
+    function PDOConnect()
+    {
+        //select user 
+        $user = "user"; //default user       
+        if(isset($_SESSION["USER_LOGGED"])) //check for admin
+        {
+            $user = $_SESSION["USER_LOGGED"];
+        }
+
+        //get credentials
+        $user_credentials = Database::GetUserCredentials($user);
+        $username = $user_credentials["Username"];
+        $password = $user_credentials["Password"];
+        $server = $user_credentials["Server"];
+        $db = $user_credentials["Database"];
+        //print_r($user_credentials);
+        
+        //connect
+        try
+        {
+            $conn = new PDO("mysql:host={$server};dbname={$db}", "{$username}", "{$password}");
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;            
+        }
+        catch(PDOException $e)
+        {
+            die("KO\n" . $e->getMessage());  
+            //return null;          
+        }        
     }
   }
 
